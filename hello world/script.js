892f1e4f29cd9ee4c0ca42f72207dbf083290eb1
Saif Uddin <admin@saifuddin.uk>
2024-04-12 15:53:57 +0100

    adding practice files

---
+++ hello world/script.js
@@ -0,0 +1,15 @@
+// alert("Hello"); alert("World!");
+
+let name, 
+    admin;
+
+name = null;
+admin = name;
+console.log(admin);
+name = 'tim'
+console.log(name);
+console.log(admin);
+
+let test = confirm('YES / NO')
+
+console.log(test)
