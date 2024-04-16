5f9e3eac84e9f4a340ba97898acc7a9b3894c0f0
Saif Uddin <admin@saifuddin.uk>
2024-04-16 15:55:33 +0100

    function expressions lesson

---
+++ JavaScript-Fundamentals/function expressions.js
@@ -0,0 +1,21 @@
+/* function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+}
+  
+ask(
+    "1. Do you agree?",
+    function() { alert("You agreed."); },
+    function() { alert("You canceled the execution."); }
+);
+
+ask(
+    "2. Do you agree?",
+    () => alert("You agreed."),
+    () => alert("You canceled the execution.")
+); */
+
+/* // Function Expression
+let sum = function(a, b) {
+    return a + b;
+}; */
